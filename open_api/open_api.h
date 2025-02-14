53f856f62afbc5d4ccbb51dd77cda2f1ebd978ea
Brian Grande <101441595+B-grande@users.noreply.github.com>
2025-02-14 00:21:05 -0500

    Delete open_api directory

--- open_api/open_api.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPENAI_API_H
-#define OPENAI_API_H
-
-#include <string>
-
-// This function sends a prompt to the ChatGPT API and returns the response.
-std::string callChatGPTAPI(const std::string &prompt);
-
-#endif // OPENAI_API_H
